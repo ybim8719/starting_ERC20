0a616f9dcde65922d7bde82c8dec26c39874cea5
Pascal Thao <45816121+ybim8719@users.noreply.github.com>
2025-08-08 23:24:20 +0200

    Create MyToken.sol

---
+++ MyToken.sol
@@ -0,0 +1,39 @@
+
+
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+
+contract GLDToken is ERC20 {  
+
+}
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
